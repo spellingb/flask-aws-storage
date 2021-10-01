7f4fe78c05462bebf75fe36f087cd3f80f008073
Brandon Spell <brandon.spell@rackspace.com>
2021-10-01 11:54:16 -0500

    Change Bucket Definition logic

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 from flask import Flask, render_template, request, redirect, send_file
-from s3_functions import list_files, upload_file, show_image
+from s3_functions import create_bucket, filter_bucket, list_bucket, list_files, upload_file, show_image
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
-BUCKET = "lats-image-data"
+try:
+    BUCKET = os.environ['BUCKET_NAME']
+except:
+    ALLBUCKETS = list_bucket()
+    BUCKET = filter_bucket(ALLBUCKETS)
+    if BUCKET == None:
+        create_bucket()
+        BUCKET = list_bucket
+        
 
 @app.route("/")
 def home():
@@ -24,6 +32,9 @@ def upload():
         f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
         upload_file(f"uploads/{f.filename}", BUCKET)
         return render_template('index.html')
+@app.route('/verify')
+def verify():
+        return '<p>' + BUCKET + '</p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
